770b0baf78e4295ebe9935e1dcf91f76e4cd3e1b
vincenz8 <115328178+vincenz8@users.noreply.github.com>
2025-05-07 15:29:39 +0200

    Small fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require 'model/dtasks-db.php';
 require 'model/ftasks-db.php';
 require 'model/points-db.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['update'])) {
 
     if (isset($_POST['local_tasks']) && check_array_structure($_POST['local_tasks'])) {
         $localTasks = sanitize_tasks($_POST['local_tasks']);
@@ -41,9 +41,9 @@ if ($fetchedTasks && date("Y-m-d", strtotime($fetchedTasks[0]['task_date'])) !==
     $fetchedTasks = [];
 }
 
-$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+$action = htmlspecialchars($_POST['action']);
 if (!$action) {
-    $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+    $action = htmlspecialchars($_GET['action']);
     if (!$action) {
         $action = 'manage-tasks'; // assigning default value if NULL or FALSE
     }
