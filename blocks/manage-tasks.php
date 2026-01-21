279f3ade535ffbe9da79f1df2fb03f04f92bd795
vincenz8 <115328178+vincenz8@users.noreply.github.com>
2026-01-21 18:11:24 +0100

    Updated textures, HTML, CSS and gitignore file

--- blocks/manage-tasks.php
+++ blocks/manage-tasks.php
@@ -1,18 +1,18 @@
 <?php include 'header.php'; ?>
-<div>
-    <form action="." method="post" id="taskForm">
-        <h3>To-do List</h3>
-        <div id="addTaskField" style="display: inline">
-            <input type="text" id="taskName" placeholder="What to do?">
-            <select id="taskWeight" name="taskWeight">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select>
-            <button type="button" id="buttonAdd">Add Task</button>
-        </div>
+<form action="." method="post" id="taskForm">
+    <div id="addTaskField" style="display: inline">
+        <input type="text" id="taskName" placeholder="What to do?">
+        <select id="taskWeight" name="taskWeight">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+        </select>
+        <button type="button" id="buttonAdd">Add Task</button>
+    </div>
+    <div class="container">
         <div id="taskList" class="task-list">
+            <h3>To-do List</h3>
             <script>
             document.addEventListener("DOMContentLoaded", function () {
             <?php foreach ($fetchedTasks as $task): 
@@ -22,8 +22,8 @@
             });
             </script>
         </div>
-        <h3>Finished Tasks</h3>
         <div id="finishedTasks" class="task-list">
+            <h3>Finished Tasks</h3>
             <script>
             document.addEventListener("DOMContentLoaded", function () {
             <?php foreach ($fetchedTasks as $task): 
@@ -33,8 +33,8 @@
             });
             </script>
         </div>
-        <input type="submit" name="update" value="Update">
-    </form>
-</div>
+    </div>
+    <input type="submit" name="update" value="Update">
+</form>
 <?php include 'footer.php';
 
