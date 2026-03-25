6f987d86ce299ea8d1dbdd2ac0d034e037a2c4be
vincenz8 <115328178+vincenz8@users.noreply.github.com>
2026-03-25 11:52:14 +0100

    CSS classes: removed 'custom' and replaced 'clickable' for 'cursor-pointer'

--- blocks/manage-tasks.php
+++ blocks/manage-tasks.php
@@ -4,9 +4,9 @@
 </span>
 <form action="." method="post" id="taskForm">
     <div id="taskMenu">
-        <input type="submit" class="custom" name="update" value="Update">
+        <input type="submit" class="cursor-pointer" name="update" value="Update">
         <div id="addTaskField">
-            <input type="text" id="taskName" class="custom" placeholder="What to do?">
+            <input type="text" id="taskName" placeholder="What to do?">
             <select id="taskWeight" name="taskWeight">
                 <option value="1">Unimportant</option>
                 <option value="2">Important but non-urgent</option>
@@ -14,22 +14,22 @@
                 <option value="4">Urgent and important</option>
             </select>
         </div>
-        <button type="button" id="buttonAdd">Add Task</button>
+        <button type="button" id="buttonAdd" class="cursor-pointer">Add Task</button>
     </div>
     <div class="task-container">
         <div id="taskList" class="task-list">
             <h3>To-do List</h3>
             <div id="weight-4l" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-4l')">Urgent and important</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-4l')">Urgent and important</h4>
             </div>
             <div id="weight-3l" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-3l')">Urgent but unimportant</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-3l')">Urgent but unimportant</h4>
             </div>
             <div id="weight-2l" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-2l')">Important but non-urgent</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-2l')">Important but non-urgent</h4>
             </div>
             <div id="weight-1l" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-1l')">Unimportant</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-1l')">Unimportant</h4>
             </div>
             <script>
             document.addEventListener("DOMContentLoaded", function () {
@@ -44,16 +44,16 @@
         <div id="finishedTasks" class="task-list">
             <h3>Finished Tasks</h3>
             <div id="weight-4r" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-4r')">Urgent and important</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-4r')">Urgent and important</h4>
             </div>
             <div id="weight-3r" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-3r')">Urgent but unimportant</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-3r')">Urgent but unimportant</h4>
             </div>
             <div id="weight-2r" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-2r')">Important but non-urgent</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-2r')">Important but non-urgent</h4>
             </div>
             <div id="weight-1r" class="task-weight-group">
-                <h4 class="clickable" onclick="toggleHideTasks('weight-1r')">Unimportant</h4>
+                <h4 class="cursor-pointer" onclick="toggleHideTasks('weight-1r')">Unimportant</h4>
             </div>
             <script>
             document.addEventListener("DOMContentLoaded", function () {
